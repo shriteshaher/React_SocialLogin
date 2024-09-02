0cf3df61051e371875a22db2c3df764154a8e51a
Shritesh Aher <shriteshaher@gmail.com>
2024-09-02 12:36:26 +0530

    One Bug

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route,Navigate } from 'react-router-dom';
 import Login from './components/Login';
 import LoginDashboard from './components/LoginDashboard'
 
@@ -9,7 +9,7 @@ function App() {
    
     <Routes>
        <Route path="/login" element={<Login />} />
-       <Route path="/" element={<Login />} />
+       <Route path="/" element={<Navigate to="/login"></Navigate>} />
        <Route path="/login_dashboard" element={<LoginDashboard></LoginDashboard>}/>
     </Routes>
     
